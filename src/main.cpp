108149b2746d105f1c81078f323da68fac80a96b
ToniValac <hours_07_stacks@icloud.com>
2023-11-24 16:49:54 +0100

    Hopefull

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532_NTAG424.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
 
 // If using the breakout with SPI, define the pins for SPI communication.
 /*
@@ -37,8 +40,15 @@
 
 // If using the breakout or shield with I2C, define just the pins connected
 // to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
-#define PN532_IRQ   (13)
-#define PN532_RESET (15)  // Not connected by default on the NFC Shield
+#define NFC_SDA    13
+#define NFC_SCL    15
+#define NFC_IRQ    26
+#define NFC_RST    25
+
+Adafruit_PN532 *nfc = NULL;
+PN532_I2C *pn532_i2c = NULL;
+PN532 *pn532 = NULL;
+NfcAdapter *nfcAdapter = NULL;
 
 // Uncomment just _one_ line below depending on how your breakout or shield
 // is connected to the Arduino:
@@ -54,82 +64,105 @@
 
 // Or use this line for a breakout or shield with an I2C connection:
 
-Adafruit_PN532* nfc = NULL;
+void scanDevices(TwoWire *w)
+{
+    uint8_t err, addr;
+    int nDevices = 0;
+    uint32_t start = 0;
+    for (addr = 1; addr < 127; addr++) {
+        start = millis();
+        w->beginTransmission(addr); delay(2);
+        err = w->endTransmission();
+        delay(10);
+        if (err == 0) {
+            nDevices++;
+            String message = "[nfcTask] I2C device found at address 0x";
+            if (addr < 16) {
+                message += "0";
+            }
+            message += String(addr, HEX);
+            Serial.println(message);
+            break;
+
+        } else if (err == 4) {
+            String message = "[nfcTask] Unknown error at address 0x";
+            if (addr < 16) {
+                message += "0";
+            }
+            message += String(addr, HEX);
+            Serial.println(message);
+        }
+    }
+    if (nDevices == 0)
+        Serial.println("[nfcTask] No I2C devices found\n");
+}
 
 void setup(void) {
     Serial.begin(115200);
-
-    Wire.begin(PN532_IRQ, PN532_RESET);
-    nfc = new Adafruit_PN532(PN532_IRQ, PN532_RESET);
+    Wire.begin(NFC_SDA, NFC_SCL);
     Wire.setClock(10000);
+    nfc = new Adafruit_PN532(NFC_SDA, NFC_SCL);
+    pn532_i2c = new PN532_I2C(Wire);
+    pn532 = new PN532(*pn532_i2c);
+    nfcAdapter = new NfcAdapter(*pn532_i2c);
     nfc->begin();
-
-    uint32_t versiondata = nfc->getFirmwareVersion();
-    if (! versiondata) {
-        Serial.print("Didn't find PN53x board");
-    }
-    else {
+    nfc->SAMConfig();
+    nfc->SAMConfig();
+    scanDevices(&Wire);
+    int error = Wire.endTransmission();
+    if (error == 0) {
+        uint32_t versiondata = nfc->getFirmwareVersion();
+        if (! versiondata) {
+            Serial.println("[nfcTask] Didn't find PN53x board");
+        }
         // Got ok data, print it out!
-        Serial.print("Found chip PN53x"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-        Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-        Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-        // configure board to read RFID tags
-        nfc->SAMConfig();
-    }
-    Serial.println("Waiting for an ISO14443A Card ...");
+        String message = "[nfcTask] Found chip PN5" + String((versiondata >> 24) & 0xFF, HEX);
+        Serial.println(message);
+        message = "[nfcTask] Firmware ver. " + String((versiondata >> 16) & 0xFF, DEC);
+        Serial.println(message);
+        message = "[nfcTask] Firmware ver. " + String((versiondata >> 8) & 0xFF, DEC);
+        Serial.println(message);
+    } else {
+        String message = "[nfcTask] Wire error: " + String(error);
+        Serial.println(message);
+    } 
 }
 
 void loop(void) {
     uint8_t success;
     uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
     uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    uint8_t data[256];                        // Buffer to store the data read from the tag
-    uint8_t bytesread = 0;                    // Variable to store the number of bytes read from the tag
-        
-    // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
-    // the UID, and uidLength will indicate the size of the UUID (normally 7)
-    Serial.println("Waiting for tag");
+
+    Serial.println("Waiting for tag...");
     success = nfc->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 420);
     
     if (success) {
-        // Display some basic information about the card
-        Serial.println("Found an ISO14443A tag");
-        Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-        Serial.print("  UID Value: ");
-        nfc->PrintHex(uid, uidLength);
-        Serial.println("");
-        
-        if ((uidLength == 7) || (uidLength == 4)) {
-            uint8_t cardType = nfc->ntag424_isNTAG424();
-            if (cardType == 1) {
-                bytesread = nfc->ntag424_ISOReadFile(data);
-                Serial.println("This is an NTAG424 tag.");
-            } else {
-                Serial.println("Not an NTAG424 tag.");
-            }
+        uint8_t cardType = nfc->ntag424_isNTAG424();
+        if (cardType) {
+            uint8_t data[256];
+            uint8_t bytesread = nfc->ntag424_ISOReadFile(data);
+            // Extract URL from data
+            String url = String((char*)data);
+            Serial.println("URL from NTAG424: " + url);
         } else {
-            return;
+            if (nfcAdapter->tagPresent()) {
+                NfcTag tag = nfcAdapter->read();
+                NdefMessage message = tag.getNdefMessage();
+                for (int i = 0; i < message.getRecordCount(); i++) {
+                    NdefRecord record = message.getRecord(i);
+                    int payloadLength = record.getPayloadLength();
+                    byte payload[payloadLength];
+                    record.getPayload(payload);
+                    // payload is a byte array containing the data from the record
+                    // Convert byte array to string
+                    String payloadAsString = "";
+                    for (int j = 0; j < payloadLength; j++) {
+                        payloadAsString += (char)payload[j];
+                    }
+                    Serial.println("Payload from NDEF: " + payloadAsString);
+                }
+            }
         }
-        
-        // Display the current page number
-        Serial.print("Response ");
-
-        // Display the results, depending on 'success'
-        if (bytesread)
-        {
-            // Dump the page data
-            data[bytesread] = 0;
-            String lnurl = (char*)data;
-            Serial.println(lnurl);
-            //nfc.PrintHexChar(data, bytesread);
-        }
-        else
-        {
-            Serial.println("Failed to read from the tag.");
-        }
-        
-        // Wait a bit before trying again
-        delay(2000);
-    } 
+    }
 }
 
